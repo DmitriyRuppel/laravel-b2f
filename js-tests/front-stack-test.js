2b643e8431c9d1a70f9e6064df779ae7659ea2c7
Дмитрий Руппель <dmitriy.ruppel@gmail.com>
2018-07-20 13:01:31 +0500

    Tests refactored

--- js-tests/front-stack-test.js
+++ js-tests/front-stack-test.js
@@ -95,11 +95,19 @@ var testWorkObject = function () {
 
         assert.ok(typeof all_data === 'object', 'Check "all()" method');
 
-        assert.strictEqual(all_data.test_string_key, 'test_value', 'Check string value');
-        assert.strictEqual(all_data.test_key_for_num_val, 1000, 'Check numeric value');
-        assert.strictEqual(all_data[88], 123, 'Check numeric value');
+        // 0 => Actual, 1 =>Expecte, 2 => Message
+        var test_data = [
+            [all_data.test_string_key, 'test_value', 'Check string value'],
+            [all_data.test_key_for_num_val, 1000, 'Check numeric value'],
+            [all_data[88], 123, 'Check numeric value'],
+            [all_data.test_null_key, null, 'Check null value']
+        ];
+
+        test_data.forEach(function(test_item) {
+            assert.strictEqual(test_item[0], test_item[1], test_item[2]);
+        });
+
         assert.deepEqual(all_data.test_object_key, window[stack_name].test_object_key, 'Check value which is an object');
-        assert.strictEqual(all_data.test_null_key, null, 'Check null value');
     });
 
     /**
@@ -124,22 +132,26 @@ var testWorkObject = function () {
      * Tests checking data exists.
      */
     QUnit.test('get()', function (assert) {
-        // Simple keys
-        assert.strictEqual(window.frontStack.get('test_string_key'), 'test_value', 'Test getting simple string value by string key');
-        assert.strictEqual(window.frontStack.get('test_key_for_num_val'), 1000, 'Test getting simple numeric value by string key');
-        assert.strictEqual(window.frontStack.get(88), 123, 'Test getting simple numeric value by numeric key');
+
+        // 0 => Key, 1 => Expected, 2 => Message
+        var test_data = [
+            ['test_string_key', 'test_value', 'Test getting simple string value by string key'],
+            ['test_key_for_num_val', 1000, 'Test getting simple numeric value by string key'],
+            [88, 123, 'Test getting simple numeric value by numeric key'],
+            ['test_null_key', null, 'Test getting null'],
+            ['test_object_key.0', 10, 'Dot-notation'],
+            ['test_object_key.1', 11, 'Dot-notation'],
+            ['test_object_key.3.test_array.0', 1, 'Dot-notation'],
+            ['test_object_key.3.test_array.1', 'array_val', 'Dot-notation'],
+            ['test_object_key.3.test_array.2', null, 'Dot-notation'],
+            ['test_object_key.test_4', 'value_4', 'Dot-notation']
+        ];
+
+        test_data.forEach(function(test_item) {
+            assert.strictEqual(window.frontStack.get(test_item[0]), test_item[1], test_item[2]);
+        });
+
         assert.deepEqual(window.frontStack.get('test_object_key'), window[stack_name].test_object_key, 'Test getting object');
-        assert.strictEqual(window.frontStack.get('test_null_key'), null, 'Test getting null');
-
-        // Dot-notation
-        assert.strictEqual(window.frontStack.get('test_object_key.0'), 10, 'Dot-notation');
-        assert.strictEqual(window.frontStack.get('test_object_key.1'), 11, 'Dot-notation');
-
-        assert.strictEqual(window.frontStack.get('test_object_key.3.test_array.0'), 1, 'Dot-notation');
-        assert.strictEqual(window.frontStack.get('test_object_key.3.test_array.1'), 'array_val', 'Dot-notation');
-        assert.strictEqual(window.frontStack.get('test_object_key.3.test_array.2'), null, 'Dot-notation');
-
-        assert.strictEqual(window.frontStack.get('test_object_key.test_4'), 'value_4', 'Dot-notation');
 
         // Not default
         assert.strictEqual(window.frontStack.get('test_string_key', 'default'), 'test_value', 'Get actual data instead default value');
@@ -149,20 +161,21 @@ var testWorkObject = function () {
      * Tests checking data not exists.
      */
     QUnit.test('! get()', function (assert) {
-        // Simple keys
-        assert.strictEqual(window.frontStack.get('any_string'), undefined, 'Test getting not exists data by strung key');
-        assert.strictEqual(window.frontStack.get(100), undefined, 'Test getting not exists data by numeric key');
-        assert.strictEqual(window.frontStack.get('any_string.more'), undefined, 'Test getting not exists data by path');
+        not_exists_test_keys.forEach(function(key) {
+            assert.strictEqual(window.frontStack.get(key), undefined, 'Get by key ' + key + ' is Undefined');
+        });
     });
 
     /**
-     * Tests checking data not exists.
+     * Tests default value.
      */
     QUnit.test('get() default value', function (assert) {
-        // Simple keys
-        assert.strictEqual(window.frontStack.get('any_string', 123), 123, 'Test get default string');
-        assert.strictEqual(window.frontStack.get(100, null), null, 'Test get default null');
-        assert.strictEqual(window.frontStack.get('any_string.more', 'default'), 'default', 'Test get default string');
+        not_exists_test_keys.forEach(function(key) {
+
+            var default_value = Math.random();
+
+            assert.strictEqual(window.frontStack.get(key, default_value), default_value, 'Default value for key ' + key);
+        });
     });
 
 };
